45e90e4856a1b6dc30e1fba0156db7c8e03466a7
Fronteros <jperezsalm@uoc.edu>
2025-03-05 17:47:50 +0100

    Agregando archivos faltantes

---
+++ src/app/data/jugadores.ts
@@ -0,0 +1,43 @@
+export const JUGADORES = [
+    {
+      id: 1,
+      nombre: "Juan Pérez",
+      posicion: "Base",
+      edad: 25,
+      altura: "1.80m",
+      video: "assets/videos/juan.mp4"
+    },
+    {
+      id: 2,
+      nombre: "Carlos López",
+      posicion: "Escolta",
+      edad: 27,
+      altura: "1.85m",
+      video: "assets/videos/carlos.mp4"
+    },
+    {
+      id: 3,
+      nombre: "Miguel Torres",
+      posicion: "Alero",
+      edad: 23,
+      altura: "1.90m",
+      video: "assets/videos/miguel.mp4"
+    },
+    {
+      id: 4,
+      nombre: "Andrés Ramírez",
+      posicion: "Ala-Pívot",
+      edad: 28,
+      altura: "2.00m",
+      video: "assets/videos/andres.mp4"
+    },
+    {
+      id: 5,
+      nombre: "Luis González",
+      posicion: "Pívot",
+      edad: 26,
+      altura: "2.05m",
+      video: "assets/videos/luis.mp4"
+    }
+  ];
+  
