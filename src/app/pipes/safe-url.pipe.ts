9ad81ec3ab84cb5566a36baf2fed2239ac7b67ba
paulacmorales <paulacmorales@uoc.edu>
2025-04-09 20:12:28 +0200

    Subida nuevos videos + Agregar vídeo YT

--- src/app/pipes/safe-url.pipe.ts
+++ src/app/pipes/safe-url.pipe.ts
@@ -2,14 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'safeUrl',
+  name: 'safe',
   standalone: true
 })
-export class SafeUrlPipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) { }
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
 
-  transform(value: string): SafeResourceUrl {
-    console.log('Sanitizando URL:', value); // DEBUG
-    return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+  transform(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
